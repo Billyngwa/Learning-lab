db5fa1e4f84b577c395a5158fc4681206f091737
Billyngwa <billytatangwa5@gmail.com>
2025-07-26 23:50:23 +0100

    defining the big integer

--- PyLessons/hello.py
+++ PyLessons/hello.py
@@ -1,20 +1,13 @@
 from flask import Flask
-name = "Professor"
-# print("Hello " + name + "!")
-def greet_some_body(name_of_person:str)-> None:{
-print("Hello " + name_of_person + "!")
-}
-greet_some_body("professor")
-
 # the duncode
 app = Flask(__name__)
 app.config["DEBUG"] = True
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
+    return '<h1>Good evening And welcome to my api!</h1>'
 
-def add(a:int,b:int)-> int:
-    print("Adding " + str(a) + " and " + str(b) + ": " + str(a+b))
-    return a+b
-
-add(1,2)
+@app.route("/user/<name>")
+def user(name):
+    return f'<h1>Hello {name}!</h1>'
+if __name__ == '__main__':
+    app.run(host='0.0.0', port=7000, debug=True)
--- PyLessons/numbers.py
+++
@@ -1,7 +0,0 @@
-number:int = 32
-greaternumbers:int=100
-floating_number:float = 3.14
-complex_number:complex = 1 + 2j
-string_value:str = "Hello World"
-# from PyLessons.hello import greet_some_body, add
-print("The number is: " + str(number))
