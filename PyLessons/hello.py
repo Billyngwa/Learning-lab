42bc3f8d9d6ef6bd703a9c0c3aa6fd27e5c6eeac
Billyngwa <billytatangwa5@gmail.com>
2025-07-26 22:59:11 +0100

    defining the big integer

--- PyLessons/hello.py
+++ PyLessons/hello.py
@@ -5,6 +5,14 @@ def greet_some_body(name_of_person:str)-> None:{
 print("Hello " + name_of_person + "!")
 }
 greet_some_body("professor")
+
+# the duncode
+app = Flask(__name__)
+app.config["DEBUG"] = True
+@app.route('/')
+def index():
+    return '<h1>Hello World!</h1>'
+
 def add(a:int,b:int)-> int:
     print("Adding " + str(a) + " and " + str(b) + ": " + str(a+b))
     return a+b
